c90f15de9feaf0d2628f5dc72dfacc45e4e844f7
Joseph García Barrantes <69489728+Ghozkiu@users.noreply.github.com>
2021-10-03 14:48:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const body = document.querySelector('body');
 const label = document.querySelector('.container span');
 
 
+//just executes the event
 button.addEventListener('click', () => {
+    applyRandomColor();
+});
+
+
+//returns a string with a random color
+const generateRandomColor = () => {
     let red = Math.floor(Math.random() * 255);
     let green = Math.floor(Math.random() * 255);
     let blue = Math.floor(Math.random() * 255);
-    let color = `rgb( ${red}, ${green}, ${blue})`
+    return `rgb( ${red}, ${green}, ${blue})`
+}
+
+//apply the random color
+const applyRandomColor = () => {
+    color = generateRandomColor();
     body.style.backgroundColor = color;
     label.textContent = color;
-});
+}
+
+
+//auto generates a random color when page is loaded
+applyRandomColor();
 
 
 
